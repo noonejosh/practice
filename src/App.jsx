b2f67ff0b4a11bdc77a4f2dc592a54e0cd0496be
noonejosh <pwepwepz21@gmail.com>
2025-07-30 18:12:54 +0800

    List using props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,25 @@
 import List from './List';
 
 function App() {
-
+    const fruits = [
+        {id: 1,name:'Apple', calories: 95}, 
+        {id: 2,name:'Banana', calories: 105}, 
+        {id: 3,name:'Cherry', calories: 50}, 
+        {id: 4,name:'Date', calories: 282}, 
+        {id: 5,name:'Elderberry', calories: 73}
+    ];
+    
+    const vegetables = [
+        {id: 6,name:'Carrot', calories: 41},
+        {id: 7,name:'Broccoli', calories: 55},
+        {id: 8,name:'Spinach', calories: 23},
+        {id: 9,name:'Potato', calories: 77},
+        {id: 10,name:'Cucumber', calories: 16}
+    ];
     return(
         <>
-            <List />
+            {fruits.length > 0 && <List items={fruits} category="fruits"/> }
+            {vegetables.length > 0 ? <List items={vegetables} category="vegetables"/> : null}
         </>
     );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,24 +1,20 @@
 
 
-function List(){
-    const fruits = [
-        {id: 1,name:'Apple', calories: 95}, 
-        {id: 2,name:'Banana', calories: 105}, 
-        {id: 3,name:'Cherry', calories: 50}, 
-        {id: 4,name:'Date', calories: 282}, 
-        {id: 5,name:'Elderberry', calories: 73}];
+function List(props){
+
+    const itemList = props.items;
+    const category = props.category;
     
     //fruits.sort((a,b) => a.name.localeCompare(b.name)); //Alphabetical sort
     //fruits.sort((a,b) => a.calories - b.calories); //Sort by calories
-    fruits.sort((a,b) => b.name.localeCompare(a.name)); //Reverse alphabetical sort
+    //fruits.sort((a,b) => b.name.localeCompare(a.name)); //Reverse alphabetical sort
     //fruits.sort((a,b) => b.calories - a.calories); //Reverse sort by calories
-
     //display all fruits
-    // const listItems = fruits.map(fruit =>
-    //     <li key={fruit.id}>
-    //         {fruit.name}: &nbsp;
-    //         <b>{fruit.calories}</b></li>
-    // );
+    const listItems = itemList.map(item =>
+        <li key={item.id}>
+            {item.name}: &nbsp;
+            <b>{item.calories}</b></li>
+    );
 
     //filter low calorie fruits
     //const lowCalorieFruits = fruits.filter(fruit => fruit.calories < 100);
@@ -31,16 +27,19 @@ function List(){
     // );
 
     //filter high calorie fruits
-    const highCalorieFruits = fruits.filter(fruit => fruit.calories >= 100);
+    //const highCalorieFruits = fruits.filter(fruit => fruit.calories >= 100);
     //display high calorie fruits that is 100 calories or more
-    const listItems = highCalorieFruits.map(highCal =>
-        <li key={highCal.id}>
-            {highCal.name}: &nbsp;
-            <b>{highCal.calories}</b></li>
-    );
+    // const listItems = highCalorieFruits.map(highCal =>
+    //     <li key={highCal.id}>
+    //         {highCal.name}: &nbsp;
+    //         <b>{highCal.calories}</b></li>
+    // );
 
     return(
-        <ol>{listItems}</ol>
+        <>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-item">{listItems}</ol>
+        </>
     );
 }
 
