d345b3aa0d9ff4b4f4272ae148a27962eb5de74a
noonejosh <pwepwepz21@gmail.com>
2025-07-30 17:34:06 +0800

    conditional rendering

--- src/UserGreeting.jsx
+++ src/UserGreeting.jsx
@@ -1,10 +1,10 @@
 
 
-function UserGreeting(props) {
-    
+function UserGreeting({ isLoggedIn, username }) {
+    const welcomeMessage = <h2 className="welcome-message">Welcome {username}</h2>
+    const loginMessage = <h2 className="login-prompt">Please log in</h2>
     return(
-        props.isLoggedIn ? <h2>Welcome {props.username}</h2> : 
-                            <h2>Please log in</h2>
+        isLoggedIn ? welcomeMessage : loginMessage                     
     );
 }
 
